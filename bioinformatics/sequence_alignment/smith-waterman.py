8a9a49ff02c6c37293983e4f4430cae7c637f8ea
andsanv <sanvito.andrea@yahoo.com>
2024-04-04 16:17:35 +0200

    Completed Smith-Waterman algorithm implementation.

--- bioinformatics/sequence_alignment/smith-waterman.py
+++ bioinformatics/sequence_alignment/smith-waterman.py
@@ -20,6 +20,9 @@ def parse_args():
         calculate local sequence alignment on two strings given as input."""
     )
     parser.add_argument(
+        "-v", "--verbose", action="store_true", help="option to increase results detail"
+    )
+    parser.add_argument(
         "string1", help="first string on which to calculate sequence alignment"
     )
     parser.add_argument(
@@ -40,40 +43,7 @@ def parse_args():
     return args
 
 
-def find_previous(list, row, col) -> tuple[int, list[tuple[int, int]]]:
-    """Allows to find relationships between different cells of the matrix, which
-    are used for backtracking. That's how solutions can be found after computation.
-
-    Given a list of partial results and the coordinates of a cell, finds the
-    maximum of those results and adds to the list to return the related coordinates.
-    """
-
-    if list == None or len(list) == 0:
-        return None, None
-
-    max_value = max([x for x in list if x is not None])
-    indexes = []
-    coords = []
-
-    for i, v in enumerate(list):  # finds the indexes of the maximum values in the list
-        if v == max_value:
-            indexes.append(i)
-
-    for index in indexes:  # based on the index, adds correspondent coords to list
-        match index:
-            case 0:
-                coords.append((row - 1, col))
-            case 1:
-                coords.append((row, col - 1))
-            case 2:
-                coords.append((row - 1, col - 1))
-            case other:
-                exit(1)
-
-    return max_value, coords
-
-
-def compute(word1, word2, weights) -> tuple[list[list], dict, int]:
+def compute(word1: str, word2: str, weights: list[int]) -> tuple[list[list], dict[int, list[list[str]]]]:
     """Core function that builds the Smith-Waterman matrix, which tracks the local
     sequence alignments for every substring of the strings given in input."""
 
@@ -81,7 +51,7 @@ def compute(word1, word2, weights) -> tuple[list[list], dict, int]:
     word2 = " " + "-" + word2
 
     matrix = [[0 for _ in range(len(word2))] for _ in range(len(word1))]
-    previous = {}
+    previous_coordinates = {}
 
     matrix[0] = [char for char in word2]  # adds second word as first matrix row
     for index, row in enumerate(matrix):  # adds first as first matrix column
@@ -93,7 +63,7 @@ def compute(word1, word2, weights) -> tuple[list[list], dict, int]:
                 matrix[row][col] = 0
                 continue
 
-            options = { 0: None }
+            options = {0: None}
             # adds the cost of coming from top
             if row > 1:
                 val = int(matrix[row - 1][col]) + weights["gap"]
@@ -109,7 +79,11 @@ def compute(word1, word2, weights) -> tuple[list[list], dict, int]:
                         options[val] = [(row, col - 1)]
             # adds the cost (or reward) of coming from top-left
             if row > 1 and col > 1:
-                val = matrix[row - 1][col - 1] + (weights["match"] if word1[row] == word2[col] else weights["mismatch"])
+                val = matrix[row - 1][col - 1] + (
+                    weights["match"]
+                    if word1[row] == word2[col]
+                    else weights["mismatch"]
+                )
                 if val != 0:
                     if val in options:
                         options[val].append((row - 1, col - 1))
@@ -117,137 +91,167 @@ def compute(word1, word2, weights) -> tuple[list[list], dict, int]:
                         options[val] = [(row - 1, col - 1)]
 
             matrix[row][col] = max(options)
-            previous[(row, col)] = options[max(options)]
+            previous_coordinates[(row, col)] = options[max(options)]
 
-    return matrix, previous
-    
+    return matrix, previous_coordinates
 
-    
-def build_solutions(matrix: list[list], previous: dict, word1: str, word2: str):
+
+def build_solutions(
+    matrix: list[list], previous_coordinates: dict[tuple[int, int], list[tuple[int, int]]], word1: str, word2: str
+) -> dict[int, list[tuple[int, int]]]:
     """Creates a visual representation of the local alignment results.
     Solutions may be multiple."""
 
-    def find_starting_points(matrix: list[list]):
-        positions = {}
+    def find_starting_coords(matrix: list[list]):
+        """Returns a dictionary that maps all matrix values to the their respective
+        coordinates."""
 
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
-                if matrix[i][j] > 1:
-                    if matrix[i][j] in positions:
-                        positions[matrix[i][j]].append((i, j))
+        positions = {}  # maps matrix values (alignment scores) to coordinates
+
+        for i, row in enumerate(matrix):
+            for j, value in enumerate(row):
+                if type(value) == int and value > 1:
+                    if value in positions:
+                        positions[value].append((i, j))
                     else:
-                        positions[matrix[i][j]] = [(i, j)]
+                        positions[value] = [(i, j)]
 
         return positions
 
-    def compute_solutions(matrix, previous, row, col):
-        if matrix[row][col] == 0:  # base case
-            if row > col:
-                first = word1[0:row + 1]
-                third = ' ' * (row - col) + word2[0:col + 1]
-            elif row < col:
-                first = ' ' * (col - row) + word1[0:row + 1]
-                third = word2[0:col + 1]
-            else:
-                first = word1[0:row + 1]
-                third = word2[0:col + 1]
+    def compute_solutions(matrix, previous_coordinates, visited, row, col):
+        """\"Backtracks\" the solution, or the path that brings to that specific (row, col) position.
+        Many code lines may be unclear, but they only help to make output more readable.
 
-            second = ' ' * max(row,col)
+        To make things clearer, here's an output example:
+            Local alignment:
+            ACCCTG      # (solution[0])
+                ||      # (solution[1])
+               TTGGT    # (solution[2])
+        This allows to understand where the common sequence is located in both the inputted words.
+        """
+
+        visited[row][col] = True  # setting the current cell as visited
+
+        if matrix[row][col] == 0:  # base case of the recursive function
+            if (
+                row > col
+            ):  # starting index of the common sequence is further in first word
+                first = word1[0 : row - 1]
+                third = " " * (row - col) + word2[0 : col - 1]  # shifting word2
+            elif (
+                row < col
+            ):  # starting index of the common sequence is further in second word
+                first = " " * (col - row) + word1[0 : row - 1]  # shifting word1
+                third = word2[0 : col - 1]
+            else:  # sequence starts at the same index in both words
+                first = word1[0 : row - 1]
+                third = word2[0 : col - 1]
+
+            second = " " * (max(row, col) - 1)
+            # as the base case only tracks how the initial parts of the words are aligned,
+            # only blank spaces are needed
 
             return [[first, second, third]]
 
-        solutions = []
-        for prev_coords in previous[(row, col)]:  # for every "antecedent"
-            for s in compute_solutions(matrix, previous, prev_coords[0], prev_coords[1]):
-                if prev_coords[0] == row - 1 and prev_coords[1] == col:
-                    # adding a gap in word2
-                    s[0] += matrix[row][0]
-                    s[1] += ' '
-                    s[2] += "-"
-                elif prev_coords[0] == row and prev_coords[1] == col - 1:
-                    # adding a gap in word1
-                    s[0] += "-"
-                    s[1] += ' '
-                    s[2] += matrix[0][col]
+        solutions = []  # solutions may be multiple
+        for previous in previous_coordinates[(row, col)]:  # for every "antecedent"
+            previous_row, previous_col = previous
+
+            for solution in compute_solutions(
+                matrix, previous_coordinates, visited, previous[0], previous[1]
+            ):
+                if previous_row == row - 1 and previous_col == col:
+                    # if we added a gap in word2
+                    solution[1] += (
+                        " " if len(solution[2].strip()) != len(word2) else "*"
+                    )
+                    # solution[1] is a string that shows how the words are aligned
+                    solution[0] += matrix[row][0]  # solution[0] is the first word
+                    solution[2] += "-"  # solution[2] is the second word
+                elif previous_row == row and previous_col == col - 1:
+                    # if we added a gap in word1
+                    solution[1] += (
+                        " " if len(solution[0].strip()) != len(word1) else "*"
+                    )
+                    solution[0] += "-"
+                    solution[2] += matrix[0][col]
                 else:
-                    # keeping words as they are
-                    s[0] += matrix[row][0]
-                    s[1] += '|' if matrix[row][0] == matrix[0][col] else '*'
-                    s[2] += matrix[0][col]
+                    # if we kept words as they were
+                    solution[0] += matrix[row][0]
+                    solution[1] += "|" if matrix[row][0] == matrix[0][col] else "*"
+                    solution[2] += matrix[0][col]
 
-                solutions.append(s)  # solutions may be multiple
+                solutions.append(solution)
 
-        print(f"solutions = {solutions}")
         return solutions
 
     solutions = {}
-    positions = find_starting_points(matrix)
-    
-    visits = [[False for _ in range(len(word2) + 2)] for _ in range(len(word1) + 2)]
+    coordinates = find_starting_coords(matrix)
+    visited = [[False for _ in range(len(word2) + 2)] for _ in range(len(word1) + 2)]
+    # matrix to keep track of the visited cells
 
-    first = ""; second = ""; third = "" # first is word1, third is word2,
-                                        # second is a string containing symbols to
-                                        # better understand the alignment
-    
-    for key in positions:
-        for (row, col) in positions[key]:
-            if visits[row][col] == False:
-                print(f"row = {row}, col = {col}")
-                for s in compute_solutions(matrix, previous, row, col):
+    for matrix_value in sorted(coordinates.keys(), reverse=True):
+        for row, col in coordinates[matrix_value]:
+            if not visited[row][col]:
+                for solution in compute_solutions(
+                    matrix, previous_coordinates, visited, row, col
+                ):
                     if len(word1) - row > len(word2) - col:
-                        first = s[0] + word1[row + 1:]
-                        third = s[2] + word2[col + 1:] + ' ' * (len(word1) - row)
+                        solution[0] += word1[row - 1 :]
+                        solution[2] += word2[col - 1 :] + " " * (len(word1) - row + 1)
                     elif len(word1) - row < len(word2) - col:
-                        first = s[0] + word1[row + 1:] + ' ' * (len(word2) - col)
-                        third = s[2] + word2[col + 1:]
+                        solution[0] += word1[row - 1 :] + " " * (len(word2) - col + 1)
+                        solution[2] += word2[col - 1 :]
                     else:
-                        first = s[0] + word1[row + 1:]
-                        third = s[2] + word2[col + 1:]
-                    second = s[1] + ' ' * max(len(word1) - row, (len(word2) - col))
+                        solution[0] += word1[row - 1 :]
+                        solution[2] += word2[col - 1 :]
+                    solution[1] += " " * (max(len(word1) - row, (len(word2) - col)) + 1)
 
-                    if key in solutions:
-                        solutions[key].append(s)
+                    if matrix_value in solutions:
+                        solutions[matrix_value].append(solution)
                     else:
-                        solutions[key] = [s]
-    
-                visits[row][col] = True
+                        solutions[matrix_value] = [solution]
+
+                visited[row][col] = True
 
     return solutions
-            
 
 
-def print_results(matrix, result, solutions) -> None:
+def print_results(
+    matrix: list[list], results: dict[int, list[list[str]]], verbose: bool
+) -> None:
     """Auxiliary function used to format terminal output."""
 
-    print(f"global sequence alignment matrix:")  # computed matrix
-    for line in matrix:
-        for element in line:
-            if type(element) == str or element >= 0:
-                print(end=" ")
-            print(f"{element}\t", end="")
-        print()
+    if len(results.keys()) == 0:
+        print("no useful solutions were found.")
+    else:
+        if verbose:
+            print(f"local sequence alignment matrix:")  # computed matrix
+            for line in matrix:
+                for element in line:
+                    if type(element) == str or element >= 0:
+                        print(end=" ")
+                    print(f"{element}\t", end="")
+                print()
 
-    print(f"\n\nalignment cost: {result}", end="\n\n\n")  # calculated "cost"
+            i = 0
 
-    print("possible solutions:")  # calculated solutions
-    for i, solution in enumerate(solutions):
-        print(f"{i + 1}  --> \t", end="")
-        for c in solution[0]:
-            print(f"{c} ", end="")
-        print("\n\t", end="")
-        for c1, c2 in zip(solution[0], solution[1]):
-            if c1 == "-" or c2 == "-":
-                print("  ", end="")
-            elif c1 == c2:
-                print("| ", end="")
-            else:
-                print(". ", end="")
+            for alignment_score, solutions in results.items():
+                print(f"\n\nalignment score: {alignment_score}")
 
-        print("\n\t", end="")
-        for c in solution[1]:
-            print(f"{c} ", end="")
+                for solution in solutions:
+                    i += 1
+                    print(f"\n{i}  -->", end="")
+                    for string in solution:
+                        print(f"\t{string}")
+        else:
+            print(f"highest alignment score: {max(results.keys())}")
 
-        print()
+            print("solutions:")
+            for i, solution in enumerate(results[max(results.keys())]):
+                print(f"\n{i + 1}  -->", end="")
+                for string in solution:
+                    print(f"\t{string}")
 
     return
 
@@ -264,16 +268,10 @@ def __main__() -> None:
         int(arg) for arg in args.weights
     ]
 
-    matrix, previous = compute(word1, word2, weights)
-    solutions = build_solutions(matrix, previous, word1, word2)
+    matrix, previous_coordinates = compute(word1, word2, weights)
+    solutions = build_solutions(matrix, previous_coordinates, word1, word2)
 
-    # for key, val in solutions.items():
-    #     print(key, ":", sep=" ")
-    #     for el in val:
-    #         for inner in el:
-    #             print("\t", inner)
-
-    # print_results(matrix, result, solutions)
+    print_results(matrix, solutions, args.verbose)
 
     return
 
